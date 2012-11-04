b73c237354fcfe1dfed5330a07eb8c4bf4fee913
maciejhirsz <maciej.hirsz@gmail.com>
2012-11-04 13:43:56 +0100

    initial commit of logo animation for docs :)

---
+++ docs/js/logo.js
@@ -0,0 +1,65 @@
+
+canvas = new rippl.Canvas({
+  width: 269,
+  height: 120
+});
+
+document.getElementById('logo').appendChild(canvas.getDocumentElement());
+
+rippl.assets.preload('img/rippl.png', function(){
+
+  timer = new rippl.Timer({ fps: 60 });
+  timer.bind(canvas);
+
+  canvas.createSprite({
+    src: 'img/rippl.png',
+    x: 0,
+    y: 0,
+    width: 269,
+    height: 120,
+    anchorX: 0,
+    anchorY: 0
+  });
+
+  mask = canvas.createShape({
+    color: '#000',
+    composition: 'source-atop',
+    x: 0,
+    y: 0,
+    width: 269,
+    height: 120,
+    anchorX: 0,
+    anchorY: 0
+  });
+
+  bling = canvas.createShape({
+    alpha: 0.75,
+    color: '#fff',
+    composition: 'source-atop',
+    x: -40,
+    y: 60,
+    rotation: Math.PI / 10,
+    width: 20,
+    height: 150
+  });
+
+  mask.transform({
+    duration: 1000,
+    to: {
+      alpha: 0
+    }
+  });
+
+  bling.transform({
+    duration: 600,
+    delay: 1000,
+    transition: 'easeInOut',
+    from: {
+      x: -40
+    },
+    to: {
+      x: 300
+    }
+  })
+
+});
